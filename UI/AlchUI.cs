b84fa58dd874ca2daabb55f327a6d79c6f35064d
Exitium <luaresgames@gmail.com>
2021-04-19 00:17:03 -0300

    Actual stacking because github funky

--- UI/AlchUI.cs
+++ UI/AlchUI.cs
@@ -4,11 +4,12 @@ using Terraria.GameContent.UI.Elements;
 using Terraria.ModLoader;
 using Terraria.UI;
 using PotionOverhaul.UI.CustomSlot;
-using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace PotionOverhaul.UI
 {
+	//Overengineering, restart
 	internal class AlchUI : UIState
 	{
 		UIImage Background = new UIImage(ModContent.GetTexture("PotionOverhaul/UI/Background"));
@@ -17,6 +18,7 @@ namespace PotionOverhaul.UI
 		public UIImageButton BrewButton = new UIImageButton(ModContent.GetTexture("PotionOverhaul/UI/BrewButton"));
 		public int[] OldIngredients = new int[3];
 		public Item OldPotion = new Item();
+		public bool Blocked;
 		CroppedTexture2D backgroundTexture = new CroppedTexture2D(ModContent.GetTexture("PotionOverhaul/UI/Slot"), Color.White);
 		CroppedTexture2D iconTextureIngredient = new CroppedTexture2D(ModContent.GetTexture("PotionOverhaul/UI/IngredientSlotIcon"), Color.White);
 		CroppedTexture2D iconTexturePotion = new CroppedTexture2D(ModContent.GetTexture("PotionOverhaul/UI/PotionSlotIcon"), Color.White);
@@ -78,11 +80,17 @@ namespace PotionOverhaul.UI
 		public override void Update(GameTime gameTime)
 		{
 			base.Update(gameTime);
-			//Overengineering, restart
 			if (IngredientSlot.All(m => m.Item.IsAir) && !PotionSlot.Item.IsAir && !(PotionSlot.Item.modItem as AlchPotion).Brewed)
 			{
 				PotionSlot.Item.TurnToAir();
 				return;
+			}
+			if (Blocked)
+			{
+				if (!PotionSlot.Item.IsAir && !(PotionSlot.Item.modItem as AlchPotion).Brewed)
+				{
+					Blocked = false;
+				}
 			}
 			if (!Enumerable.SequenceEqual(OldIngredients, IngredientSlot.Select(e => e.Item.type)) || PotionSlot.Item.IsAir && IngredientSlot.Any(a => !a.Item.IsAir))
 			{
@@ -109,6 +117,10 @@ namespace PotionOverhaul.UI
 					}
 					PotionSlot.Item.alpha = 155;
 				}
+				else
+				{
+					Blocked = true;
+				}
 			}
 			OldIngredients = IngredientSlot.Select(e => e.Item.type).ToArray();
 		}
@@ -116,11 +128,46 @@ namespace PotionOverhaul.UI
 		{
 			if (!PotionSlot.Item.IsAir)
 			{
-				PotionSlot.Item.alpha = 0;
-				(PotionSlot.Item.modItem as AlchPotion).Brewed = true;
-				for (int i = 0; i < IngredientSlot.Length; i++)
+				if (Blocked)
 				{
-					IngredientSlot[i].Item.stack--;
+					return;
+				}
+				if ((PotionSlot.Item.modItem as AlchPotion).Brewed)
+				{
+					List<Item> itemList = new List<Item>();
+					for (int i = 0; i < IngredientSlot.Length; i++)
+					{
+						if (!IngredientSlot[i].Item.IsAir)
+						{
+							if (itemList.FindAll(t => t.type == IngredientSlot[i].Item.type).Count > 0)
+							{
+								itemList.Find(t => t.type == IngredientSlot[i].Item.type).stack++;
+							}
+							else
+							{
+								Item ingredient = IngredientSlot[i].Item.Clone();
+								ingredient.stack = 1;
+								itemList.Add(ingredient);
+							}
+						}
+					}
+					if (Enumerable.SequenceEqual(OldIngredients, IngredientSlot.Select(e => e.Item.type)) && !IngredientSlot.Any(e => e.Item.stack == 0))
+					{
+						PotionSlot.Item.stack++;
+						for (int i = 0; i < IngredientSlot.Length; i++)
+						{
+							IngredientSlot[i].Item.stack--;
+						}
+					}
+				}
+				else
+				{
+					PotionSlot.Item.alpha = 0;
+					(PotionSlot.Item.modItem as AlchPotion).Brewed = true;
+					for (int i = 0; i < IngredientSlot.Length; i++)
+					{
+						IngredientSlot[i].Item.stack--;
+					}
 				}
 			}
 		}
